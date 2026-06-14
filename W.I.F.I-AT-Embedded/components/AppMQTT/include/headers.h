7ea74cb65c6629e4748eb979be3fe28537263fda
ilfpns <yeomsh09@gmail.com>
2026-06-14 15:44:37 +0900

    feat: add mqtt protocol hander and log func

--- W.I.F.I-AT-Embedded/components/AppMQTT/include/headers.h
+++ W.I.F.I-AT-Embedded/components/AppMQTT/include/headers.h
@@ -5,11 +5,13 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <string.h>
-#include "esp_system.h"
-#include "nvs_flash.h"
+
 #include "esp_event.h"
 #include "esp_netif.h"
 #include "esp_log.h"
+#include "esp_system.h"
+
+#include "nvs_flash.h"
 #include "mqtt_client.h"
 
 #endif
--- W.I.F.I-AT-Embedded/components/AppMQTT/include/originFunc.h
+++ W.I.F.I-AT-Embedded/components/AppMQTT/include/originFunc.h
@@ -1,6 +1,8 @@
 #ifndef APP_BLE_ORIGIN_FUNC
 #define APP_BLE_ORIGIN_FUNC
 
+#include "headers.h"
 
+static void mqtt5_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
 
 #endif
