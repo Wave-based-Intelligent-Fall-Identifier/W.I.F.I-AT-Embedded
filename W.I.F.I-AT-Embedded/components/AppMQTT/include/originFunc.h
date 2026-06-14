49203d836bcaeacf22ab604bb8b9c6cf76127418
ilfpns <yeomsh09@gmail.com>
2026-06-14 16:15:05 +0900

    feat: add mqtt init func and edit handler event type

--- W.I.F.I-AT-Embedded/components/AppMQTT/include/originFunc.h
+++ W.I.F.I-AT-Embedded/components/AppMQTT/include/originFunc.h
@@ -4,5 +4,7 @@
 #include "headers.h"
 
 static void mqtt5_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
+static void log_error_if_nonzero(const char *message, int error_code);
+static void print_user_property(mqtt5_user_property_handle_t user_property);
 
 #endif
